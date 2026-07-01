c955bc3d73f8e90dd0674897a3ee0e04a7470362
Kiran Bhatta <bhattakiran1111@gmail.com>
2026-07-02 00:45:19 +0545

    Add circle drawing program in LAB_3_.c
    Implement Midpoint Circle Drawing Algorithm using graphics.

--- LAB_3_.c
+++ LAB_3_.c
@@ -1,1 +1,56 @@
+#include <graphics.h>
+#include <stdio.h>
+#include <conio.h>
 
+void drawCircle(int xc, int yc, int x, int y)
+{
+    putpixel(xc + x, yc + y, WHITE);
+    putpixel(xc - x, yc + y, WHITE);
+    putpixel(xc + x, yc - y, WHITE);
+    putpixel(xc - x, yc - y, WHITE);
+    putpixel(xc + y, yc + x, WHITE);
+    putpixel(xc - y, yc + x, WHITE);
+    putpixel(xc + y, yc - x, WHITE);
+    putpixel(xc - y, yc - x, WHITE);
+}
+
+void main()
+{
+    int gd = DETECT, gm;
+    int xc, yc, r;
+    int x = 0, y;
+    int p;
+
+    initgraph(&gd, &gm, "..\\bgi");
+
+    printf("Midpoint Circle Drawing Algorithm\n");
+    printf("This Program is coded by Kiran Bhatta\n");
+
+    printf("Enter center coordinates (xc yc): ");
+    scanf("%d %d", &xc, &yc);
+
+    printf("Enter radius: ");
+    scanf("%d", &r);
+
+    y = r;
+    p = 1 - r;
+
+    while (x <= y)
+    {
+        drawCircle(xc, yc, x, y);
+
+        if (p < 0)
+            p = p + 2 * x + 3;
+        else
+        {
+            p = p + 2 * (x - y) + 5;
+            y--;
+        }
+
+        x++;
+        delay(50);
+    }
+
+    getch();
+    closegraph();
+}
