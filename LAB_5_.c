5c91d226a7b500436c913f18ee01df780b3acf7a
Kiran Bhatta <bhattakiran1111@gmail.com>
2026-07-04 11:41:34 +0545

    Add boundary fill algorithm in LAB_5_.c
    Implement boundary fill algorithm to fill shapes.

--- LAB_5_.c
+++ LAB_5_.c
@@ -1,1 +1,40 @@
 
+#include <graphics.h>
+#include <stdio.h>
+#include <conio.h>
+
+void boundaryFill(int x, int y, int fillColor, int boundaryColor)
+{
+    int currentColor;
+
+    currentColor = getpixel(x, y);
+
+    if (currentColor != boundaryColor && currentColor != fillColor)
+    {
+        putpixel(x, y, fillColor);
+
+        boundaryFill(x + 1, y, fillColor, boundaryColor);
+        boundaryFill(x - 1, y, fillColor, boundaryColor);
+        boundaryFill(x, y + 1, fillColor, boundaryColor);
+        boundaryFill(x, y - 1, fillColor, boundaryColor);
+    }
+}
+
+void main()
+{
+    int gd = DETECT, gm;
+
+    initgraph(&gd, &gm, "..\\bgi");
+
+    printf("Boundary Fill Algorithm\n");
+    printf("This Program is coded by Kiran Bhatta\n");
+
+    /* Draw a Rectangle */
+    rectangle(150, 100, 350, 250);
+
+    /* Fill the Rectangle */
+    boundaryFill(200, 150, RED, WHITE);
+
+    getch();
+    closegraph();
+}
