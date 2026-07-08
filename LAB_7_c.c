c800ca6c4c88a2d907363feaf053e434aead6bec
Kiran Bhatta <bhattakiran1111@gmail.com>
2026-07-08 13:06:55 +0545

    Implement 2D scaling for rectangles in LAB_7_c.c
    This program performs 2D scaling on a rectangle based on user input.

--- LAB_7_c.c
+++ LAB_7_c.c
@@ -1,1 +1,28 @@
+#include <graphics.h>
+#include <stdio.h>
+#include <conio.h>
 
+void main()
+{
+    int gd = DETECT, gm;
+    int x1, y1, x2, y2;
+    float sx, sy;
+
+    initgraph(&gd, &gm, "..\\bgi");
+
+    printf("2D Scaling\n");
+    printf("This Program is coded by Kiran Bhatta\n");
+
+    printf("Enter x1 y1 x2 y2: ");
+    scanf("%d%d%d%d", &x1, &y1, &x2, &y2);
+
+    printf("Enter scaling factors (sx sy): ");
+    scanf("%f%f", &sx, &sy);
+
+    rectangle(x1, y1, x2, y2);
+
+    rectangle(x1 * sx, y1 * sy, x2 * sx, y2 * sy);
+
+    getch();
+    closegraph();
+}
