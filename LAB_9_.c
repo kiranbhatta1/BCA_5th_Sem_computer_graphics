86668a63f4e2d86ad3df48fc9dc995c66474cc23
Kiran Bhatta <bhattakiran1111@gmail.com>
2026-07-11 10:22:08 +0545

    Refactor LAB_9_ for better readability and functionality
    Updated the Cohen Sutherland Line Clipping Algorithm program with improved formatting and added a new line.

--- LAB_9_.c
+++ LAB_9_.c
@@ -1,12 +1,23 @@
 #include <graphics.h>
 #include <stdio.h>
 #include <conio.h>
-void main(){
-  int gb = DETECT, gm;
-initgraph (&gd, &gm, "..\\bgi");
-printf("CohenSutherland Line clipping Algorithm\n");
-printf("This Program is code By Kiran Bhatta");
- rectangle(150 ,100,450, 300);
-line (50 ,50,500,350);
-setColor(RED);
-lin
+
+void main()
+{
+    int gd = DETECT, gm;
+
+    initgraph(&gd, &gm, "..\\bgi");
+
+    printf("Cohen Sutherland Line Clipping Algorithm\n");
+    printf("This Program is coded by Kiran Bhatta\n");
+
+    rectangle(150,100,450,300);
+
+    line(50,50,500,350);
+
+    setcolor(RED);
+    line(150,125,425,275);
+
+    getch();
+    closegraph();
+}
