ded4d2e456446de857f4a04cc37c43765e33b71f
Kiran Bhatta <bhattakiran1111@gmail.com>
2026-07-11 10:20:37 +0545

    Add initial graphics program using Cohen-Sutherland
    Implement basic graphics program with line clipping.

---
+++ LAB_9_.c
@@ -0,0 +1,12 @@
+#include <graphics.h>
+#include <stdio.h>
+#include <conio.h>
+void main(){
+  int gb = DETECT, gm;
+initgraph (&gd, &gm, "..\\bgi");
+printf("CohenSutherland Line clipping Algorithm\n");
+printf("This Program is code By Kiran Bhatta");
+ rectangle(150 ,100,450, 300);
+line (50 ,50,500,350);
+setColor(RED);
+lin
