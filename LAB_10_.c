604a0ddd6997aa5e3a97c43d623b3c2c2412c61c
Kiran Bhatta <bhattakiran1111@gmail.com>
2026-07-12 07:10:42 +0545

    Add polygon clipping visualization in LAB_10_.c
    Implement Sutherland Hodgeman Polygon Clipping Algorithm with graphics.

--- LAB_10_.c
+++ LAB_10_.c
@@ -1,1 +1,31 @@
+#include <graphics.h>
+#include <stdio.h>
+#include <conio.h>
 
+void main()
+{
+    int gd = DETECT, gm;
+
+    initgraph(&gd, &gm, "..\\bgi");
+
+    printf("Sutherland Hodgeman Polygon Clipping Algorithm\n");
+    printf("This Program is coded by Kiran Bhatta\n");
+
+    rectangle(150,100,450,300);
+
+    setcolor(WHITE);
+    line(100,150,250,50);
+    line(250,50,500,200);
+    line(500,200,300,350);
+    line(300,350,100,150);
+
+    setcolor(RED);
+    line(150,117,250,100);
+    line(250,100,450,200);
+    line(450,200,350,300);
+    line(350,300,150,200);
+    line(150,200,150,117);
+
+    getch();
+    closegraph();
+}
